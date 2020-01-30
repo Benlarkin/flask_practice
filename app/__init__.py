58f764b60e9e0c610b7f01c4cf1a4a74fee335ca
Benjamin Larkin <Ben>
2020-01-30 12:54:12 -0800

    initialized database, version control

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from app.configuration import Configuration
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
+
 def index():
     user = {'username' : 'Ben'}
     posts = [
@@ -13,7 +15,17 @@ def index():
             'author': {'username': 'Bennyboo'},
             'body' : 'Beyond the Permafrost!'
         }
+        
 
     ]
-    
-    return render_template('index.html', title='Home', user =user, posts = posts)
+    test = 'yagah'
+    return render_template('index.html', title='Home', user =user, posts = posts, test = test)
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title = 'Sign In', form = form)
