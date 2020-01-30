1ea667c3590648f806ef19433f9947fc2221e9bf
Benjamin Larkin <Ben>
2020-01-30 15:02:41 -0800

    reset project... pt 4 databases finished

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,30 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
-
 def index():
-    user = {'username' : 'Ben'}
+    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
-            'body' : 'Beautiful day in Bellingham!'
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Bennyboo'},
-            'body' : 'Beyond the Permafrost!'
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
-        
-
     ]
-    test = 'yagah'
-    return render_template('index.html', title='Home', user =user, posts = posts, test = test)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
-
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title = 'Sign In', form = form)
+    return render_template('login.html',  title='Sign In', form=form)
